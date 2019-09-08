79f5e2454eef69d9dc8be40861408f2dc0b4c952
JustinHartman407 <50879948+JustinHartman407@users.noreply.github.com>
2019-09-08 16:43:04 -0400

    Add files via upload
    This is an imported data file assignment

---
+++ ISMBIPythonimportdatafile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+Location = "datasets/all_040_in_12.P1.csv"
+df = pd.read_csv(Location, header=None)
+
+df.head()
+
+
+# In[3]:
+
+
+import pandas as pd
+
+Location = "datasets/all_040_in_12.P1.csv"
+df = pd.read_csv(Location,)
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
